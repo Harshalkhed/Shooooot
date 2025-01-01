1d4c5a18cd586eb77d15c4a4be8b3bde1c15f105
Tushar Satpute <tusharsatpute68@gmail.com>
2025-01-01 17:01:09 +0530

    First commit

--- Assets/Shooooot/Scripts/GameManager.cs
+++ Assets/Shooooot/Scripts/GameManager.cs
@@ -174,4 +174,8 @@ public class GameManager : MonoBehaviour {
             Debug.Log(PlayerPrefs.GetString("AudioSetting", "-"));
         }
     }
+
+    public void GOHome() {
+        SceneManager.LoadSceneAsync(1);
+    }
 }
